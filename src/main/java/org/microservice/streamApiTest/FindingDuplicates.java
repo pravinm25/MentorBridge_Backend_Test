a86a0d5314d2f9b2ebf57f910118d988762307f0
pravinm25 <pravinmuruganantham123@gmail.com>
2026-03-02 20:47:56 +0530

    added

--- src/main/java/org/microservice/streamApiTest/FindingDuplicates.java
+++ src/main/java/org/microservice/streamApiTest/FindingDuplicates.java
@@ -1,12 +1,20 @@
 package org.microservice.streamApiTest;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FindingDuplicates {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(10,15,8,49,25,98,98,32,15);
-        numbers.stream().distinct().forEach(System.out::println);
+        Set<Integer> duplicateOnes = new HashSet<>();
+        Set<Integer> duplicates = numbers.stream()
+                .filter(n -> !duplicateOnes.add(n))
+                .collect(Collectors.toSet());
+        System.out.println(duplicates);
     }
 }
+
 
